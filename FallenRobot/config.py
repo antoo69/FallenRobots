2b9e32060bd3bfba8adc69ce5ed4d47ac5c0a0bb
antoo69 <antobarry69@gmail.com>
2024-08-10 13:02:21 +0700

    Update config.py
    Signed-off-by: antoo69 <antobarry69@gmail.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph//file/6d5e0605baae809d3e1ec.jpg"
+    START_IMG = "https://telegra.ph/file/e93428e2725f8c475c31d.mp4"
 
     SUPPORT_CHAT = "BestieVirtual"  # Your Telegram support group chat username where your users will go and bother you
 
